e6d21937d5669497bdd4a920e72dc2bf057f22e2
Joe Raio <joe.raio@microsoft.com>
2017-08-07 16:01:27 -0400

    Connects to Azure IoT Hub to Receive Messages

--- index.js
+++ index.js
@@ -1,25 +1,55 @@
+'use strict';
+var clientFromConnectionString = require('azure-iot-device-mqtt').clientFromConnectionString;
+var Message = require('azure-iot-device').Message;
 var exec = require('nexecp').exec;
+require('dotenv').config();
 
-// TODO: Connect Azure IoT Hub
+// Get messages from Azure IoT Hub
+// https://docs.microsoft.com/en-us/azure/iot-hub/iot-hub-node-node-c2d
 
-// Call the Blink Tool
-blink("1,6,8,1");
+var iotHost = process.env.IOT_HOST;
+var deviceKey = process.env.DEVICE_KEY;
+var deviceId = process.env.DEVICE_ID;
+
+var connectionString = 'HostName=' + iotHost +';DeviceId=' + deviceId + ';SharedAccessKey=' + deviceKey;
+var client = clientFromConnectionString(connectionString);
+
+function printResultFor(op) {
+  return function printResult(err, res) {
+    if (err) console.log(op + ' error: ' + err.toString());
+    if (res) console.log(op + ' status: ' + res.constructor.name);
+  };
+}
+
+var connectCallback = function (err) {
+  if (err) {
+    console.log('Could not connect: ' + err);
+  } else {
+    console.log('Client connected');
+    client.on('message', function (msg) {
+        console.log('Id: ' + msg.messageId + ' Body: ' + msg.data);
+        client.complete(msg, printResultFor('completed'));
+        // Call the Blink Tool
+        blink("1,6,8,1");      
+    });    
+  }
+};
+
+client.open(connectCallback);
 
 // Let's Blink
 function blink(pattern) {
 
     exec('blink1-tool --play ' + pattern).then(function(out) {
-    console.log(out.stdout, out.stderr);
+        console.log(out.stdout, out.stderr);
     }, function(err) {
-    console.error(err);
+        console.error(err);
     });
 
 }
 
 /*
-
-https://github.com/todbot/blink1/releases
-
-https://github.com/todbot/blink1/blob/master/docs/blink1-mk2-tricks.md
-
+    Blink(1) Documentation and Tooling
+    https://github.com/todbot/blink1/releases
+    https://github.com/todbot/blink1/blob/master/docs/blink1-mk2-tricks.md
 */
